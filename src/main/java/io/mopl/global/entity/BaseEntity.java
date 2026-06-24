277518446b21db0844e9f292a5ce76f74da00ef7
한성재 <anis15740@gmail.com>
2026-06-24 15:38:56 +0900

    feat: Add BaseEntity

---
+++ src/main/java/io/mopl/global/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package io.mopl.global.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  protected UUID id;
+
+  @JsonIgnore
+  @CreatedDate
+  @Column(updatable = false)
+  protected Instant createdAt;
+}
+
