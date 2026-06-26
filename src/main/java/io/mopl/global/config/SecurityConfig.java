0f8ccb69562b9aada5fc1f4ad4735f0d442b7508
한성재 <anis15740@gmail.com>
2026-06-26 18:29:14 +0900

    feat: SecurityFilterChain 기본 뼈대 구현

--- src/main/java/io/mopl/global/config/SecurityConfig.java
+++ src/main/java/io/mopl/global/config/SecurityConfig.java
@@ -1,20 +1,70 @@
 package io.mopl.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(this::configureCsrf)
+
+        .formLogin(this::configureFormLogin)
+
+        .httpBasic(this::configureHttpBasic)
+
+        .sessionManagement(this::configureSessionManagement)
+
+        .authorizeHttpRequests(this::configureAuthorizeRequests)
+
+    ;
+
+    return http.build();
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  private void configureCsrf(CsrfConfigurer<HttpSecurity> csrf) {
+    csrf.disable();
+  }
+
+  private void configureFormLogin(FormLoginConfigurer<HttpSecurity> login) {
+    login.disable();
+  }
+
+  private void configureHttpBasic(HttpBasicConfigurer<HttpSecurity> basic) {
+    basic.disable();
+  }
+
+  private void configureSessionManagement(SessionManagementConfigurer<HttpSecurity> session) {
+    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  private void configureAuthorizeRequests(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
+    auth
+        .anyRequest().permitAll();
   }
 }
