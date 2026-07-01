d08cfce21752e436f256a80a32ee526802c7dbfd
한성재 <anis15740@gmail.com>
2026-07-01 18:38:15 +0900

    fix: ErrorCode rollback

--- src/main/java/io/mopl/global/exception/ErrorCode.java
+++ src/main/java/io/mopl/global/exception/ErrorCode.java
@@ -22,10 +22,10 @@ public enum ErrorCode {
 
   // auth
   INVALID_EMAIL(HttpStatus.NOT_FOUND, "AUTH_404", "이메일이 일치하지 않습니다."),
-  INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "AUTH_403", "비밀번호가 일치하지 않습니다."),
+  INVALID_PASSWORD(HttpStatus.NOT_FOUND, "AUTH_403", "비밀번호가 일치하지 않습니다."),
 
-  // mail
-  MAIL_SEND_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "MAIL_500", "이메일 전송에 실패했습니다"),
+  // 참여자 파악 여부
+  NOT_CHAT_PARTICIPANT(HttpStatus.FORBIDDEN, "CHAT_403","채팅방 참여자가 아닙니다."),
 
   // Direct Message
   SELF_CONVERSATION_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "DM_400", "자기 자신과는 대화를 생성할 수 없습니다"),
