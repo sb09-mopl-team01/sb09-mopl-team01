5e3ce71823c7a6381a04971ecda6ec73166185b0
hyemin <hyemin.ellen.97@gmail.com>
2026-07-02 10:19:22 +0900

    fix: ErrorCode 중복코드 제거

--- src/main/java/io/mopl/global/exception/ErrorCode.java
+++ src/main/java/io/mopl/global/exception/ErrorCode.java
@@ -26,6 +26,8 @@ public enum ErrorCode {
   // Direct Message
   SELF_CONVERSATION_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "DM_400", "자기 자신과는 대화를 생성할 수 없습니다"),
   CONVERSATION_NOT_FOUND(HttpStatus.NOT_FOUND, "DM_404", "대화를 찾을 수 없습니다"),
+  CONVERSATION_CREATE_RACE_CONDITION(HttpStatus.CONFLICT, "DM_409", "대화 생성 중 충돌이 발생했습니다"),
+
   // auth
   INVALID_EMAIL(HttpStatus.NOT_FOUND, "AUTH_404", "이메일이 일치하지 않습니다."),
   INVALID_PASSWORD(HttpStatus.NOT_FOUND, "AUTH_403", "비밀번호가 일치하지 않습니다."),
@@ -36,10 +38,6 @@ public enum ErrorCode {
   // 플레이리스트
   PLAYLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "PLAYLIST_404", "플레이리스트를 찾을 수 없습니다"),
   PLAYLIST_CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "PLAYLIST_404", "플레이리스트에 해당 콘텐츠가 없습니다"),
-  // Direct Message
-  SELF_CONVERSATION_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "DM_400", "자기 자신과는 대화를 생성할 수 없습니다"),
-  CONVERSATION_NOT_FOUND(HttpStatus.NOT_FOUND, "DM_404", "대화를 찾을 수 없습니다"),
-  CONVERSATION_CREATE_RACE_CONDITION(HttpStatus.CONFLICT, "DM_409", "대화 생성 중 충돌이 발생했습니다"),
 
   // 리뷰
   ALREADY_REVIEWED(HttpStatus.CONFLICT, "REVIEW_409", "이미 작성한 리뷰가 존재합니다.")
