d0416cd590187e80f56df93b19a82ed7a22fb0b1
Hyemin <101932925+hyemin-L@users.noreply.github.com>
2026-07-08 17:48:09 +0900

    [BE-09] feat: directmessgae 테스트케이스 추가 (#72)

--- src/main/java/io/mopl/domain/directmessage/entity/DirectMessage.java
+++ src/main/java/io/mopl/domain/directmessage/entity/DirectMessage.java
@@ -4,6 +4,7 @@ import io.mopl.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
