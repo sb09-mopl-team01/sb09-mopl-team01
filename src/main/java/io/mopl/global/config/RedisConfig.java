dd1e6def404fb50f3113b93d0a4db872c138fe4f
devseongjun <jseong013@gmail.com>
2026-07-20 19:53:36 +0900

    fix: ElastiCache TLS 연결 설정 반영

--- src/main/java/io/mopl/global/config/RedisConfig.java
+++ src/main/java/io/mopl/global/config/RedisConfig.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class RedisConfig {
@@ -21,11 +24,21 @@ public class RedisConfig {
   @Value("${spring.data.redis.password}")
   private String password;
 
+  @Value("${spring.data.redis.ssl.enabled:false}")
+  private boolean sslEnabled;
+
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    var redisStandaloneConfiguration = new org.springframework.data.redis.connection.RedisStandaloneConfiguration(host, port);
-    redisStandaloneConfiguration.setPassword(password);
-    return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    var redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+    if (StringUtils.hasText(password)) {
+      redisStandaloneConfiguration.setPassword(password);
+    }
+
+    var clientConfiguration = sslEnabled
+        ? LettuceClientConfiguration.builder().useSsl().build()
+        : LettuceClientConfiguration.builder().build();
+
+    return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfiguration);
   }
 
   @Bean
