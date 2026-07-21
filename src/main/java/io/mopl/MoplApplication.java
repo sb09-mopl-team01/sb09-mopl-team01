231192b9515de7e9e2c9cbf886303d18304b7a19
한성재 <anis15740@gmail.com>
2026-07-21 13:50:59 +0900

    feat: OpenSearch 설정 추가

--- src/main/java/io/mopl/MoplApplication.java
+++ src/main/java/io/mopl/MoplApplication.java
@@ -3,9 +3,15 @@ package io.mopl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    ElasticsearchDataAutoConfiguration.class,
+    ElasticsearchRepositoriesAutoConfiguration.class
+})
 public class MoplApplication {
 
   public static void main(String[] args) {
