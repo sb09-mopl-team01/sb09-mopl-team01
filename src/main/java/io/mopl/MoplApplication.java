8d04e298428899766de6d443f5fdbde85e57c3df
한성재 <anis15740@gmail.com>
2026-07-23 16:11:14 +0900

    feat: AsyncConfig에 userEventExecutor 추가

--- src/main/java/io/mopl/MoplApplication.java
+++ src/main/java/io/mopl/MoplApplication.java
@@ -1,15 +1,10 @@
 package io.mopl;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication(exclude = {
     ElasticsearchDataAutoConfiguration.class,
     ElasticsearchRepositoriesAutoConfiguration.class
