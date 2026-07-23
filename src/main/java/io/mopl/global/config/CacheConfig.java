5f96bfc643f73efbd84436abbdb84596b7d56c13
한성재 <anis15740@gmail.com>
2026-07-23 13:34:32 +0900

    fix: 테스트 코드 오류로 인한 CacheConfig 롤백

--- src/main/java/io/mopl/global/config/CacheConfig.java
+++ src/main/java/io/mopl/global/config/CacheConfig.java
@@ -2,13 +2,10 @@ package io.mopl.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -20,7 +17,7 @@ import java.time.Duration;
 public class CacheConfig {
 
   @Bean
-  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+  public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
     ObjectMapper redisObjectMapper = objectMapper.copy();
 
     BasicPolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
@@ -33,7 +30,7 @@ public class CacheConfig {
 
     redisObjectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.EVERYTHING);
 
-    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+    return RedisCacheConfiguration.defaultCacheConfig()
         .prefixCacheNameWith("mopl:")
         .entryTtl(Duration.ofHours(1))
         .disableCachingNullValues()
@@ -45,9 +42,5 @@ public class CacheConfig {
                 new GenericJackson2JsonRedisSerializer(redisObjectMapper)
             )
         );
-
-    return RedisCacheManager.builder(redisConnectionFactory)
-        .cacheDefaults(redisCacheConfiguration)
-        .build();
   }
 }
