c1fdb08fc3acb94f3f5e21e87330d72138eb65e8
한성재 <anis15740@gmail.com>
2026-07-24 15:02:07 +0900

    fix: 유저 리스트 정렬 + 검색 수정

--- src/main/java/io/mopl/domain/user/dto/request/UserCreateRequest.java
+++ src/main/java/io/mopl/domain/user/dto/request/UserCreateRequest.java
@@ -2,13 +2,18 @@ package io.mopl.domain.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserCreateRequest(
     @NotBlank(message = "이메일은 필수 입력값입니다.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     String email,
-    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d).{8,20}$",
+        message = "비밀번호는 영문자와 숫자를 무조건 포함하여 8~20자리로 입력해주세요."
+    )
     String password,
     @NotBlank(message = "이름은 필수 입력값입니다.")
     @Size(min = 2, max = 10, message = "이름은 2자 이상 10자 이하로 입력해주세요.")
